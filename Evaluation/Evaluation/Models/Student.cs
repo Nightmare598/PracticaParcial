23a8c87568e1644f286b80c772074dbd5589b213
Sebastian Morales Yabeta <masterrol001@hotmail.com>
2019-09-27 16:37:44 -0400

    Commit ERROR

---
+++ Evaluation/Evaluation/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webEval.Models
+{
+    public enum GenderType { Male, Female, Helicopter }
+    public class Student
+    {
+        [Key]
+        public string Name { get; set; }
+       [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+        [Required]
+        public GenderType Gender { get; set; }
+    }
+}
